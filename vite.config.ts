544a17ff24f7c813da2b4e7efc4219c2ed1f950f
manboxtw-png <manboxtw@gmail.com>
2026-05-19 17:13:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Minimalist-Pomodoro-Timer/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
